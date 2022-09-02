8ccb4372e316df28503657296aabe6db174558fc
linhdq <linhdq@vng.com.vn>
2022-09-02 15:31:06 +0700

    Restructure folder

--- test-automation/src/main/java/heyrin/dto/ProductStatus2.java
+++
@@ -1,7 +0,0 @@
-package heyrin.dto;
-
-public enum ProductStatus2 {
-
-    SOLD_OUT,
-    AVAILABLE;
-}
--- test-automation/src/main/java/heyrin/service/dto/ImagePurpose.java
+++
@@ -1,5 +0,0 @@
-package heyrin.service.dto;
-
-public enum ImagePurpose {
-    MAIN, SUB, MAIN_COVER, SUB_COVER
-}
